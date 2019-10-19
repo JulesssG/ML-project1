28643e4fef4b942a032afbc1832457dea98b4d64
Jules Gottraux <jules.gottraux@epfl.ch>
2019-10-19 15:47:36 +0200

    Add build_poly to implementations

--- implementations.py
+++ implementations.py
@@ -2,6 +2,18 @@
 
 import numpy as np
 
+"""
+FEATURE EXPANSION
+"""
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    N = x.shape[0]
+    matrix = np.ones((N, 1))
+    for i in range(1, degree+1):
+        for feat in x.T:
+            matrix = np.hstack((matrix, feat.reshape((N, 1))**i))
+    
+    return matrix
 
 """
 LINEAR REGRESSION
@@ -19,7 +31,7 @@ def compute_loss_mse(y, tx, w):
     mse: The mean square error
     """
     e = y - tx @ w
-    mse = (e @ e) / (2 * e.shape[0])
+    mse = (e.T @ e) / (2 * e.shape[0])
 
     return mse
 
@@ -42,7 +54,7 @@ def compute_gradient_mse(y, tx, w):
     return gradient
 
 
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
+def gradient_descent(y, tx, initial_w, max_iters, gamma, verbose=False):
     """
     Compute the linear regression using gradient descent
 
@@ -63,6 +75,8 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
         gradient = compute_gradient_mse(y, tx, w)
         
         w = w - gamma * gradient
+        if n_iter % 100 == 0:
+            print(compute_loss_mse(y, tx, w))
 
     loss = compute_loss_mse(y, tx, w)
 
